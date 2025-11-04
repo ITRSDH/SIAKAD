ea0e0bbe476756c88f28662a96928fb0b86705a8
ITRSDH <itrsdianhusada@gmail.com>
2025-11-04 18:02:42 +0700

    first commit

--- config/api.php
+++ config/api.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'base_url' => env('API_BASE_URL', 'http://localhost:8001/api/v1/', 'http://10.101.10.253:8001/api/v1/'),
+    // 'base_url' => env('API_BASE_URL', 'http://localhost:8001/api/v1/'),
+    'base_url' => env('API_BASE_URL', 'https://apistikesdh.alvion.id/api/v1/'),
 ];
